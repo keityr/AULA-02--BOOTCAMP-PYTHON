a7832a06e4cf02dbaa52ad083467e6a03d723d5e
KeityR <ranna.keity@gmail.com>
2025-04-01 19:37:01 -0300

    Exercicios de Python

---
+++ exercicios.py
@@ -0,0 +1,134 @@
+#Inteiros (int)
+#Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+
+
+#try:
+  # num1 = int(input("Insira um número: "))
+    
+  #  num2 = int(input("Insira outro número:"))
+ #   soma = num1 + num2 
+ #   print(soma)
+#except ValueError:
+#    print("O número não é inteiro")
+
+
+#Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+#num_usuario = float(input("Insira um número: "))
+
+#calculo = num_usuario % 5
+
+#print(calculo)
+
+#Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro 
+# pelo segundo.
+
+#num1 = int(input("Insira um número: "))
+#um2 = int(input("insira um segundo número: "))
+
+#divisao = num1 // num2
+
+#print(divisao)
+
+#Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+#num = int(input("Insira um número: "))
+
+#quadrado = num ** 2
+
+#print(quadrado)
+
+#Números de Ponto Flutuante (float)
+
+#Escreva um programa que receba dois números flutuantes e realize sua adição.
+#num1 = float(input("Digite um numero: "))
+#num2 = float(input("Digite outro numero:"))
+
+#soma = num1 + num2
+
+#print(soma)
+
+
+#Crie um programa que calcule a média de dois números flutuantes fornecidos pelo 
+# usuário.
+
+#numeros = [float(input(f"Digite o {i}° número com casa decimal: ")) for i in range(1, 4)]
+
+#tam_numeros = len(numeros) 
+
+#soma = sum(numeros)
+
+#media =  soma / tam_numeros
+
+#print(media)
+
+#Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+#base = int(input("Digite a base: "))
+
+#expoente = int(input("Digite o expoente: "))
+
+
+#potencia = base ** expoente
+
+#print(potencia)
+
+#Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+#celsius = float(input("Diga a temperatura em Celsius: "))
+#fahrenheit = celsius * 1.8 + 32
+
+#print(fahrenheit)
+
+
+
+#Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+#raio = 5
+
+#pi = 3.1416
+
+#area = pi * raio ** 2
+
+#print(f"{area:.2f}")
+
+#Strings (str)
+#Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+#palavra = str(input("Digite uma palavra: "))
+#maiuscula = palavra.upper()
+
+#print(maiuscula)
+
+
+#Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+#nome_completo = str(input("Digite seu nome completo: ")).lower()
+
+#print(nome_completo)
+
+
+#Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase
+# sem espaços em branco no início e no final.
+#frase =str(input("Escreva uma frase: ")).replace(" ", "")
+#print(frase)
+
+
+#Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, 
+# em seguida, imprima o dia, o mês e o ano separadamente.
+
+#mes = str(input("Escreva uma data(dd/mm/aaaa): ")).split("/")
+
+#print(mes)
+
+#Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+#primeira = str(input("Digite uma palavra: "))
+#segunda = str(input("Digite uma segunda palavra: "))
+#print(primeira+segunda)
+
+#Booleanos (bool)
+
+
+
+
+#Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+#Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+#Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+#Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
